ec5dccbd52eae520c40b82341d0506602c11aa74
Schuyler Conover <schuyler.conover@qgenda.com>
2023-06-28 09:40:34 -0400

    Run with multiple concurrent threads to simulate issue. Apply mutex to ensure single threaded behavior (see console lines that it is actually running single-threaded). Issue occurs consistently after several threads have completed.

---
+++ InstantJson.cs
@@ -0,0 +1,60 @@
+﻿using InstantJSONExample.Helper;
+using PSPDFKit;
+using PSPDFKit.Providers;
+
+namespace InstantJSONExample
+{
+    public class InstantJson
+    {
+        private static Mutex psPdfKitImportDocumentAndSaveMutexLock = new Mutex();
+
+        public void RunInstantJsonConcurrently()
+        {
+            Thread[] threads = new Thread[300];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(this.ApplyInstantJson);
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private void ApplyInstantJson()
+        {
+            var docStream = File.Open(DocumentHelper.GetAssetPath("default.pdf"), FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            var document = new Document(new StreamDataProvider(docStream));
+
+            psPdfKitImportDocumentAndSaveMutexLock.WaitOne();
+            try
+            {
+                Console.WriteLine("Obtained lock and started writing JSON");
+                var jsonStream = File.Open(DocumentHelper.GetAssetPath("instant.json"), FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                document.ImportDocumentJson(new StreamDataProvider(jsonStream));
+
+                Random rnd = new Random();
+                var tempPath = Path.Combine(Path.GetTempPath(), "default" + rnd.Next(10000000).ToString() + ".pdf");
+                using (var tempStream = File.Create(tempPath))
+                {
+                    document.SaveAs(new StreamDataProvider(tempStream), new DocumentSaveOptions());
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Releasing lock");
+                psPdfKitImportDocumentAndSaveMutexLock.ReleaseMutex();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿using System.Diagnostics;
-using PSPDFKit;
-using PSPDFKit.Providers;
-using System.Drawing.Imaging;
-using InstantJSONExample.Helper;
-
-namespace InstantJSONExample
+﻿namespace InstantJSONExample
 {
     class Test
     {
@@ -12,18 +6,8 @@ namespace InstantJSONExample
         {
             PSPDFKit.Sdk.InitializeTrial();
 
-            for (var i = 0; i < 100; i++)
-            {
-                var docStream = File.Open(DocumentHelper.GetAssetPath("default.pdf"), FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-                var document = new Document(new StreamDataProvider(docStream));
-
-                var jsonStream = File.Open(DocumentHelper.GetAssetPath("instant.json"), FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-                document.ImportDocumentJson(new StreamDataProvider(jsonStream));
-
-                var tempPath = Path.Combine(Path.GetTempPath(), "default.pdf");
-                var tempStream = File.Open(tempPath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-                document.SaveAs(new StreamDataProvider(tempStream), new DocumentSaveOptions());
-            }
+            var test = new InstantJson();
+            test.RunInstantJsonConcurrently();
             Console.WriteLine("Done!");
         }
     }
